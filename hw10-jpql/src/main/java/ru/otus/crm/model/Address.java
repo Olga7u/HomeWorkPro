b18cefaefbd37d5e03e521e264050019db62102c
Olga7u <olga7u@gmail.com>
2024-01-16 00:21:03 +0300

    Jpql

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,35 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street", length = 50, nullable = false)
+    private String street;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Address(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,37 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number", length = 20, nullable = false)
+    private String number;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
